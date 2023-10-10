c4179ae1695e9827d9a65b73c5b3ff3838448b54
Rodrigo Gouveia <rgestevao@gmail.com>
2023-10-10 19:46:24 -0300

    feat: editing create user flow

--- src/main/java/br/com/rocketseat/todolist/user/UserController.java
+++ src/main/java/br/com/rocketseat/todolist/user/UserController.java
@@ -1,5 +1,9 @@
 package br.com.rocketseat.todolist.user;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
+@Log4j2
 public class UserController {
 
-    @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
+    @Autowired
+    private IUserRepository userRepository;
 
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+        if(user != null) {
+            log.info("Usuário já existe.");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
